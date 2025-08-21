e891134e09eed6d8b44f823fd9eec11f0d0c47a5
Jakub Ducho <87933202+Ghost9887@users.noreply.github.com>
2025-08-21 13:31:43 +0200

    Update camera.c

--- Game/src/camera/camera.c
+++ Game/src/camera/camera.c
@@ -3,7 +3,7 @@
 void initCamera(Camera2D *camera, Player *player){
   camera->zoom = 1.0f;
   camera->rotation = 0.0f;
-  camera->offset = (Vector2){ SCREENWIDTH / 2.0f, SCREENHEIGHT / 2.0f }; // center of screen
+  camera->offset = (Vector2){ SCREENWIDTH / 2.0f, SCREENHEIGHT / 2.0f };
   camera->target = (Vector2){ player->x + player->width / 2, player->y + player->height / 2 };
 
 }
